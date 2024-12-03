da56a4ddbe0de1e6f5dcdb9cdf2fd48981726b6b
Matthew Herod <matthew.herod@myunidays.com>
2024-12-03 16:02:38 +0000

    chore: rename webpack config to .cjs

---
+++ webpack.config.cjs
@@ -0,0 +1,43 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    background: './src/background.ts',
+    content: './src/content.ts',
+    popup: './src/popup.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/popup.html', to: 'popup.html' },
+        {
+          from: 'scripts/icons/*.svg',
+          to: ({ context, absoluteFilename }) => {
+            const basename = path.basename(absoluteFilename, '.svg');
+            return `${basename}.png`;
+          },
+        },
+      ],
+    }),
+  ],
+};
